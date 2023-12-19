35b8693a99aaae3cc2a87d169462d65df0ac8043
Kelvin Reid <codesmithk@gmail.com>
2023-12-19 15:33:45 -0500

    Update Mesh.swift

--- Metal-Prototype/Textures/Mesh.swift
+++ Metal-Prototype/Textures/Mesh.swift
@@ -9,63 +9,73 @@ import Foundation
 import simd
 import MetalKit
 
+// Represents a set of face indices (vertex, texture coordinate, normal)
 struct FaceIndices : Hashable, Equatable {
 	var v : Int
 	var t : Int
 	var n : Int
+    
+    // Hashing for conforming to Hashable protocol
 	var hashValue : Int {
 		return "\(v),\(t),\(n)".hash
 	}
 }
 
+// Equatability for conforming to Equatable protocol
 func ==(lhs: FaceIndices, rhs: FaceIndices) -> Bool {
 	return (lhs.v == rhs.v && lhs.n == rhs.n && lhs.t == rhs.t)
 }
 
+// Represents a 3D vertex with position, normal, tangent, bitangent, and texture coordinates
 struct Vertex {
-	var position = float3(0.0,0.0,0.0)
-	var normal = float3(0.0,0.0,0.0)
-	var tangent = float3(0.0,0.0,0.0)
-	var bitangent = float3(0.0,0.0,0.0)
-	var texcoords = float2(0.0,0.0)
+	var position = SIMD3<Float>(0.0,0.0,0.0)
+	var normal = SIMD3<Float>(0.0,0.0,0.0)
+	var tangent = SIMD3<Float>(0.0,0.0,0.0)
+	var bitangent = SIMD3<Float>(0.0,0.0,0.0)
+	var texcoords = SIMD2<Float>(0.0,0.0)
 }
 
+// Represents a 3D mesh with vertices, indices, and Metal buffers
 class Mesh {
-	
 	private var vertices : [Vertex] = []
 	private var indices : [uint32] = []
+    
+    // Metal buffers
 	public var vertexBuffer : MTLBuffer? = nil
 	public var indexBuffer : MTLBuffer? = nil
-	public var indexCount : Int {
+	
+    // Computed property for the number of indices
+    public var indexCount : Int {
 		get {
 			return indices.count
 		}
 	}
 	
-	
+    // Initializes the mesh from an OBJ file at the given URL
 	init?(url : URL) {
+        // Parsing OBJ file and populating vertex and index data
 		guard let stringContent = try? String(contentsOf: url) else {
 			print("Error")
 			return nil
 		}
 		let lines = stringContent.components(separatedBy: CharacterSet.newlines)
 		
-		var positions : [float3] = []
-		var normals : [float3] = []
-		var uvs : [float2] = []
+		var positions : [SIMD3<Float>] = []
+		var normals : [SIMD3<Float>] = []
+		var uvs : [SIMD2<Float>] = []
 		var faces : [FaceIndices] = []
 		
 		for line in lines {
 			if (line.hasPrefix("v ")){//Vertex
-				var components = line.components(separatedBy: CharacterSet.whitespaces)
-				positions.append( float3( Float(components[1])!, Float(components[2])!, Float(components[3])! ) )
+				let components = line.components(separatedBy: CharacterSet.whitespaces)
+				positions.append( SIMD3<Float>( Float(components[1])!, Float(components[2])!, Float(components[3])! ) )
 			} else if (line.hasPrefix("vt ")) {//UV coords
-				var components = line.components(separatedBy: CharacterSet.whitespaces)
-				uvs.append( float2( Float(components[1])!, Float(components[2])! ) )
+				let components = line.components(separatedBy: CharacterSet.whitespaces)
+				uvs.append( SIMD2<Float>( Float(components[1])!, Float(components[2])! ) )
 				
 			} else if (line.hasPrefix("vn ")) {//Normal coords
-				var components = line.components(separatedBy: CharacterSet.whitespaces)
-				normals.append( float3( Float(components[1])!, Float(components[2])!, Float(components[3])! ) )
+				let components = line.components(separatedBy: CharacterSet.whitespaces)
+				normals.append( SIMD3<Float>( Float(components[1])!, Float(components[2])!, Float(components[3])! ) )
 			} else if (line.hasPrefix("f ")) {//Face with vertices/uv/normals
 				let components = line.components(separatedBy: CharacterSet.whitespaces)
 				// Split each face 3 indices
@@ -116,8 +126,10 @@ class Mesh {
 		print("OBJ: loaded. \(indices.count/3) faces, \(positions.count) vertices, \(normals.count) normals, \(uvs.count) texcoords.")
 	}
 	
+    // Centers and scales the mesh to fit within a unit cube
 	public func centerAndUnit(){
-		var centroid = float3(0.0,0.0,0.0)
+        // Translation and scaling logic to center and fit the mesh within a unit cube
+		var centroid = SIMD3<Float>(0.0,0.0,0.0)
 		
 		for i in 0..<vertices.count {
 			centroid += vertices[i].position;
@@ -137,37 +149,38 @@ class Mesh {
 		
 		maxi = (maxi == 0.0 ? 1.0 : maxi)
 		
-		// Scale the mesh.
+		// Scale the mesh
 		for i in 0..<vertices.count {
 			vertices[i].position *= (1.0/maxi)
 		}
 	}
 	
-	public func computeTangentFrame(){
+    // Computes the tangent and bitangent for each vertex in the mesh
+	public func computeTangentFrame() {
+        // Tangent frame computation logic for normal mapping
 		if indices.isEmpty || vertices.isEmpty {
-			// Missing data, or not the right mode (Points).
 			return;
 		}
 		
-		// Then, compute both vectors for each face and accumulate them.
+		// Then, compute both vectors for each face and accumulate them
 		for face in 0..<(indices.count/3) {
 			let v0 = vertices[Int(indices[3*face  ])]
 			let v1 = vertices[Int(indices[3*face+1])]
 			let v2 = vertices[Int(indices[3*face+2])]
 			
 			
-			// Delta positions and uvs.
+			// Delta positions and uvs
 			let deltaPosition1 = v1.position - v0.position
 			let deltaPosition2 = v2.position - v0.position
 			let deltaUv1 = v1.texcoords - v0.texcoords
 			let deltaUv2 = v2.texcoords - v0.texcoords
 			
-			// Compute tangent and binormal for the face.
+			// Compute tangent and binormal for the face
 			let det = 1.0 / (deltaUv1.x * deltaUv2.y - deltaUv1.y * deltaUv2.x)
 			let tangent = det * (deltaPosition1 * deltaUv2.y   - deltaPosition2 * deltaUv1.y)
 			let bitangent = det * (deltaPosition2 * deltaUv1.x   - deltaPosition1 * deltaUv2.x)
 			
-			// Accumulate them. We don't normalize to get a free weighting based on the size of the face.
+			// Accumulate them. We don't normalize to get a free weighting based on the size of the face
 			vertices[Int(indices[3*face  ])].tangent += tangent
 			vertices[Int(indices[3*face+1])].tangent += tangent
 			vertices[Int(indices[3*face+2])].tangent += tangent
@@ -176,7 +189,7 @@ class Mesh {
 			vertices[Int(indices[3*face+1])].bitangent += bitangent
 			vertices[Int(indices[3*face+2])].bitangent += bitangent
 		}
-		// Finally, enforce orthogonality and good orientation of the basis.
+		// Finally, enforce orthogonality and good orientation of the basis
 		for i in 0..<vertices.count {
 			vertices[i].tangent = normalize(vertices[i].tangent - vertices[i].normal * dot(vertices[i].normal, vertices[i].tangent))
 			if dot(cross(vertices[i].normal, vertices[i].tangent), vertices[i].bitangent) < 0.0 {
@@ -187,7 +200,9 @@ class Mesh {
 
 	}
 	
-	public func setupBuffers(device : MTLDevice){
+    // Sets up Metal buffers for the vertices and indices
+	public func setupBuffers(device : MTLDevice) {
+        // Creating Metal buffers for vertices and indices
 		vertexBuffer = device.makeBuffer(bytes: &vertices, length: vertices.count * MemoryLayout<Vertex>.stride, options: [])
 		indexBuffer = device.makeBuffer(bytes: &indices, length: indices.count * MemoryLayout<uint32>.stride, options: [])
 	}
